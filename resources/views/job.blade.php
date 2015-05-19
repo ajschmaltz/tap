f5bdb0f23ba275fb8adf76dcc830f81e7a17b2c0
luscadigital <joey@luscadigital.com>
2015-05-18 21:29:04 -0400

    spacing

--- resources/views/job.blade.php
+++ resources/views/job.blade.php
@@ -9,8 +9,25 @@
   <div class="section">
     <div class="row screen">
       <div class="col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-sm-10 col-sm-push-1">
-        <h1 class="text-center">Take a photo of the tree you need removed.</h1>
+        <h1 class="text-center">Get a quote for tree removal. Here's what we want to know:</h1>
         <hr/>
+        <ul>
+          <li class="lead">Take a photo of the tree you need removed</li>
+          <li class="lead">Take a photo of any obsticals we need to know about</li>
+          <li class="lead">Mark the location of the job</li>
+          <li class="lead">Provide your contact information</li>
+        </ul>
+      </div>
+    </div>
+    <footer onclick="fullpage.moveSectionDown();">
+      START
+    </footer>
+  </div>
+
+  <div class="section">
+    <div class="row screen">
+      <div class="col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-sm-10 col-sm-push-1">
+        <h1 class="text-center">Take a photo of the tree you need removed.</h1>
         <div id="fine-uploader">
         </div>
         <div class="text-center">
@@ -30,7 +47,6 @@
     <div class="row screen">
       <div class="col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-sm-10 col-sm-push-1">
         <h1 class="text-center">Take a photo of any obvious obstacles.</h1>
-        <hr/>
         <div class="text-center">
           <div class="gallery"></div>
           <div id="two">
@@ -48,7 +64,6 @@
     <div class="row screen">
       <div class="col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-sm-10 col-sm-push-1">
         <h1 class="text-center">Where is the job?</h1>
-        <hr/>
         <div id="map-canvas" style="height: 300px;"></div>
       </div>
     </div>
@@ -61,7 +76,6 @@
     <div class="row screen">
       <div class="col-lg-6 col-lg-push-3 col-md-8 col-md-push-2 col-sm-10 col-sm-push-1">
         <h1 class="text-center">Get a Quote</h1>
-        <hr/>
         <input class="form-control" type="text" placeholder="Enter your phone number" />
       </div>
     </div>
